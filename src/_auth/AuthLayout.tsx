4c1a5510bb523bdd7b6300dd92c3607ec1e9abe9
Your Name <severus.key308@gmail.com>
2023-11-01 00:22:57 +0400

    00'22

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -7,7 +7,17 @@ const AuthLayout = () => {
     return (
         <>
             {
-                isAuthenticated ? (<Navigate to="/" />) : (<section> <Outlet /> </section>)
+                isAuthenticated ? (
+                    <Navigate to="/" />
+                ) : (
+                    <>
+                        <section className='flex flex-1 justify-center items-center flex-col py-10'>
+                            <Outlet />
+                        </section>
+
+                        <img src="/assets/images/side-img.svg" alt="logo" className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
+                    </>
+                )
             }
         </>
     )
