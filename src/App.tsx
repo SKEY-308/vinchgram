a9df7cd6d1d92ee95822547318c581ee114fade8
Your Name <severus.key308@gmail.com>
2023-10-31 20:30:20 +0400

    20'30

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
-import Home from './_root/pages/Home';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RoutLayout from './_root/RoutLayout';
 
 
 const App = () => {
@@ -9,10 +12,17 @@ const App = () => {
         <main className="flex h-screen">
             <Routes>
                 {/* public routes */}
-                <Route path="/sign-in" element={<SigninForm />} />
+                <Route element={<AuthLayout />}>
+                    <Route path="/sign-in" element={<SigninForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
+                </Route>
+
 
                 {/* private routes */}
-                <Route index element={<Home />} />
+                <Route element={<RoutLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+
             </Routes>
         </main>
     )
