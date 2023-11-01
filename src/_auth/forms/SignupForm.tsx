547b58e294ddd2be1fd7b0b7eb16d81a5bc664a6
Your Name <severus.key308@gmail.com>
2023-11-01 16:29:26 +0400

    16'29

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,36 @@
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
+
+
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
+
 
 const SignupForm = () => {
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+
     return (
         <div>
             <Button>Click me</Button>
