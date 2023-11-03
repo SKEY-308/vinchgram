631282edf9b043b03348a1537c527adba452bdd2
Your Name <severus.key308@gmail.com>
2023-11-03 19:08:57 +0400

    19'08

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,1 +1,11 @@
-import { client, Account, Databases, Storage, Avatars } from "appwrite";
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
