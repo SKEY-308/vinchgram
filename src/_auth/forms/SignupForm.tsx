b3bbcefe7fc8f04357b30f3083fb03f62833ebe6
Your Name <severus.key308@gmail.com>
2023-11-03 07:19:32 +0400

    07'19

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -38,30 +38,34 @@ const SignupForm = () => {
         <Form {...form}>
 
             <div className="sm:w-420 flex-center flex-col">
-                <img className="pb-4" src="/assets/images/logo.svg" alt="logo" />
+                <img src="/assets/images/logo.svg" alt="logo" />
 
-                <h2 className="h3-bold md:h2-bold">Create a new account</h2>
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+                <p className="text-light-3 small-medium md:base-regular mt-2">To use VinchGram enter your details</p>
+
+
+
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="shadcn" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    This is your public display name.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+
             </div>
-
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
         </Form>
     )
 }
