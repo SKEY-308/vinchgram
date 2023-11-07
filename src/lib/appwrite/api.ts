a6347026af2ce74b6f8235e3447c1195a681bca3
Your Name <severus.key308@gmail.com>
2023-11-07 12:27:12 +0400

    12'27

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,7 @@
 import { ID } from "appwrite";
 
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -12,9 +12,43 @@ export async function createUserAccount(user: INewUser) {
       user.name
     );
 
-    return newAccount;
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      imageUrl: avatarUrl,
+      username: user.username,
+    });
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    // return error;
   }
 }
