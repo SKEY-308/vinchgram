2d5c53e4df884971a6fcbf46c7f57608753e4850
Your Name <severus.key308@gmail.com>
2023-11-10 15:25:01 +0400

    15'24

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -7,13 +7,13 @@ import { useToast } from "@/components/ui/use-toast"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { SignupValidation } from "@/lib/validation";
+import { SigninValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
 import { Link, useNavigate } from "react-router-dom";
 
 // import { createUserAccount } from "@/lib/appwrite/api";
-import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useSignInAccount } from "@/lib/react-query/queriesAndMutations";
 import { useUserContext } from "@/context/AuthContext";
 
 
@@ -21,23 +21,23 @@ import { useUserContext } from "@/context/AuthContext";
 
 
 
-const SignupForm = () => {
+const SigninForm = () => {
     const { toast } = useToast()
     const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
     const navigate = useNavigate();
 
     // const isLoading = false;
-    const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
+    // const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
 
-    const { mutateAsync: signInAccount, isPending: isSigningIn } = useSignInAccount();
+    const { mutateAsync: signInAccount, isPending } = useSignInAccount();
 
 
     // 1. Define your form.
-    const form = useForm<z.infer<typeof SignupValidation>>({
-        resolver: zodResolver(SignupValidation),
+    const form = useForm<z.infer<typeof SigninValidation>>({
+        resolver: zodResolver(SigninValidation),
         defaultValues: {
-            name: "",
-            username: "",
+            // name: "",
+            // username: "",
             email: "",
             password: "",
         },
@@ -45,15 +45,7 @@ const SignupForm = () => {
 
 
     // 2. Define a submit handler.
-    async function onSubmit(values: z.infer<typeof SignupValidation>) {
-        // Create the user
-        const newUser = await createUserAccount(values);
-
-        if (!newUser) {
-            return toast({
-                title: "Sign up failed. Please try again.",
-            })
-        }
+    async function onSubmit(values: z.infer<typeof SigninValidation>) {
 
         const session = await signInAccount({
             email: values.email,
@@ -86,13 +78,13 @@ const SignupForm = () => {
             <div className="sm:w-420 flex-center flex-col">
                 <img src="/assets/images/logo.svg" alt="logo" />
 
-                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-                <p className="text-light-3 small-medium md:base-regular mt-2">To use VinchGram, please enter your details</p>
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your account</h2>
+                <p className="text-light-3 small-medium md:base-regular mt-2">Welcome Back!! Please enter your details</p>
 
 
 
                 <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-                    <FormField
+                    {/* <FormField
                         control={form.control}
                         name="name"
                         render={({ field }) => (
@@ -117,7 +109,7 @@ const SignupForm = () => {
                                 <FormMessage />
                             </FormItem>
                         )}
-                    />
+                    /> */}
                     <FormField
                         control={form.control}
                         name="email"
@@ -145,19 +137,19 @@ const SignupForm = () => {
                         )}
                     />
                     <Button type="submit" className="shad-button_primary">
-                        {isCreatingAccount ? (
+                        {isUserLoading ? (
                             <div className="flex-center gap-2"> <Loader /> Loading ....</div>
-                        ) : 'Sign up'}
+                        ) : 'Sign in'}
                     </Button>
 
                     <p className="text-small-regular text-light-2 text-center mt-2">
-                        Already have account ..? <Link to='/sign-in' className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+                        Don't have an account ..? <Link to='/sign-up' className="text-primary-500 text-small-semibold ml-1">Sign up</Link>
                     </p>
                 </form>
             </div>
 
-        </Form>
+        </Form >
     )
 }
 
-export default SignupForm
+export default SigninForm
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,3 +8,12 @@ export const SignupValidation = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters." }),
 });
+
+export const SigninValidation = z.object({
+  // name: z.string().min(2, { message: "Too short" }),
+  // username: z.string().min(2, { message: "Too short" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." }),
+});
