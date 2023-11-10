88622e7bb922c32dfa3bf65b1e653bcf1413df79
Your Name <severus.key308@gmail.com>
2023-11-10 21:13:16 +0400

    21'13

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RoutLayout from './_root/RoutLayout';
 import { Toaster } from "@/components/ui/toaster"
+import Explore from './_root/pages/Explore';
+import Saved from './_root/pages/Saved';
+import AllUsers from './_root/pages/AllUsers';
+import CreatePost from './_root/pages/CreatePost';
+import EditPost from './_root/pages/EditPost';
+import PostDetails from './_root/pages/PostDetails';
+import Profile from './_root/pages/Profile';
+import UpdateProfile from './_root/pages/UpdateProfile';
 
 
 const App = () => {
@@ -22,6 +30,14 @@ const App = () => {
                 {/* private routes */}
                 <Route element={<RoutLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id/*" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />
                 </Route>
             </Routes>
 
