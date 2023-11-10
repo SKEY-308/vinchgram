9574376cb368dd3012dbf010e08f2bfa170738c9
Your Name <severus.key308@gmail.com>
2023-11-10 14:15:23 +0400

    14'15

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -60,9 +60,9 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
 
     useEffect(() => {
-        // localStorage.getItem('cookieFallback') === null
         if (
-            localStorage.getItem('cookieFallback') === '[]'
+            localStorage.getItem('cookieFallback') === '[]' ||
+            localStorage.getItem('cookieFallback') === null
         ) navigate('/sign-in')
 
         checkAuthUser();
