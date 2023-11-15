cd0d622b52986c2cb6274c330af37f492802598a
Your Name <severus.key308@gmail.com>
2023-11-15 21:33:46 +0400

    21'33

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -28,7 +28,7 @@ const Home = () => {
         <div className="flex flex-1">
             <div className="home-container">
                 <div className="home-posts">
-                    <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+                    <h2 className="h3-bold md:h2-bold text-left w-full">Feeds</h2>
                     {isPostLoading && !posts ? (
                         <Loader />
                     ) : (
