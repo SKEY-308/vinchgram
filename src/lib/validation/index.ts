e09e72189c9c32ad75184f4d90534f6ba6fb8663
Your Name <severus.key308@gmail.com>
2023-11-17 21:43:42 +0400

    21'43

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -18,6 +18,16 @@ export const SigninValidation = z.object({
     .min(8, { message: "Password must be at least 8 characters." }),
 });
 
+export const ProfileValidation = z.object({
+  file: z.custom<File[]>(),
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  username: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters." }),
+  email: z.string().email(),
+  bio: z.string(),
+});
+
 // ============================================================
 // POST
 // ============================================================
