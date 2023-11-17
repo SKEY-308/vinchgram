69397972d7259881746305bac9d3c355783f7895
Your Name <severus.key308@gmail.com>
2023-11-17 22:01:36 +0400

    22'01

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -37,7 +37,7 @@ const UpdateProfile = () => {
 
     // Queries
     const { data: currentUser } = useGetUserById(id || "");
-    const { mutateAsync: updateUser, isLoading: isLoadingUpdate } =
+    const { mutateAsync: updateUser, isPending: isLoadingUpdate } =
         useUpdateUser();
 
     if (!currentUser)
