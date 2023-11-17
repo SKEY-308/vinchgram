776b997b9c209b49641737c1bce5590d6b3ebed5
Your Name <severus.key308@gmail.com>
2023-11-17 19:04:08 +0400

    19'03

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -17,8 +17,8 @@ const PostDetails = () => {
     const { id } = useParams();
     const { user } = useUserContext();
 
-    const { data: post, isLoading } = useGetPostById(id);
-    const { data: userPosts, isLoading: isUserPostLoading } = useGetUserPosts(
+    const { data: post, isPending } = useGetPostById(id);
+    const { data: userPosts, isPending: isUserPostLoading } = useGetUserPosts(
         post?.creator.$id
     );
     const { mutate: deletePost } = useDeletePost();
@@ -49,7 +49,7 @@ const PostDetails = () => {
                 </Button>
             </div>
 
-            {isLoading || !post ? (
+            {isPending || !post ? (
                 <Loader />
             ) : (
                 <div className="post_details-card">
